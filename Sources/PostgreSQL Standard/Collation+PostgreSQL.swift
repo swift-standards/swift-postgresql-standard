e8fd4144f79c2d0e27ae28f01612b44835563c14
Coen ten Thije Boonkkamp <coen@coenttb.com>
2026-08-11 12:41:19 +0200

    Add PostgreSQL C and POSIX collations

---
+++ Sources/PostgreSQL Standard/Collation+PostgreSQL.swift
@@ -0,0 +1,9 @@
+import Structured_Queries_Primitives
+
+extension Collation {
+    /// The built-in `C` collation.
+    public static let C = Self(rawValue: "C")
+
+    /// The built-in `POSIX` collation.
+    public static let POSIX = Self(rawValue: "POSIX")
+}
